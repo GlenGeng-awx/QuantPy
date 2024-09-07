010a72f875bf72747c18bdad8224d0fc26837429
Glen Geng <glen.geng@airwallex.com>
2024-09-07 14:02:58 +0800

    2024-09-07. combine computation of both long and short

--- gringotts/giant_model.py
+++ gringotts/giant_model.py
@@ -7,9 +7,8 @@ from features import FEATURE_BUF
 
 
 class TinyModel:
-    def __init__(self, stock_df: pd.DataFrame, switch: list[bool], direction: str):
+    def __init__(self, stock_df: pd.DataFrame, switch: list[bool]):
         self.stock_df = stock_df
-        self.direction = direction
 
         self.switch = switch
         self.abbr = ', '.join([str(i) for i in range(len(switch)) if switch[i]])
@@ -17,8 +16,11 @@ class TinyModel:
 
         self.indices = []
 
-        self.successful_trades = 0
-        self.successful_rate = 0
+        self.successful_long_trades = 0
+        self.successful_long_rate = 0
+
+        self.successful_short_trades = 0
+        self.successful_short_rate = 0
 
     def check(self, idx) -> bool:
         for i, feature in enumerate(FEATURE_BUF):
@@ -31,24 +33,28 @@ class TinyModel:
             if self.check(idx):
                 self.indices.append(idx)
 
+        # evaluate the model
         valid_trade_num = 0
+        step = 5
         close = self.stock_df['close']
 
         for idx in self.indices:
-            if idx + 5 in close:
+            if idx + step in close:
                 valid_trade_num += 1
             else:
                 continue
 
-            if self.direction == 'long' and close[idx] < close[idx + 5]:
-                self.successful_trades += 1
+            if close[idx] < close[idx + step]:
+                self.successful_long_trades += 1
 
-            if self.direction == 'short' and close[idx] > close[idx + 5]:
-                self.successful_trades += 1
+            if close[idx] > close[idx + step]:
+                self.successful_short_trades += 1
 
         if valid_trade_num == 0:
             return
-        self.successful_rate = self.successful_trades / valid_trade_num * 100
+
+        self.successful_long_rate = self.successful_long_trades / valid_trade_num * 100
+        self.successful_short_rate = self.successful_short_trades / valid_trade_num * 100
 
 
 def enumerate_switches(size: int) -> list[list]:
@@ -86,7 +92,7 @@ def shrink_models(models: list[TinyModel]) -> list[TinyModel]:
 
 
 def show_models(stock_df: pd.DataFrame, fig: go.Figure, models: list[TinyModel], color: str):
-    for model in models[:10]:
+    for model in models[:15]:
         indices = model.indices
         dates = stock_df.loc[indices]['Date']
         close = stock_df.loc[indices]['close']
@@ -111,26 +117,26 @@ class GiantModel:
         self.long_models = []
         self.short_models = []
 
-    def _run(self, direction: str, models: list[TinyModel]):
+    def run(self):
         switches = enumerate_switches(len(FEATURE_BUF))
 
         for i, switch in enumerate(switches):
-            model = TinyModel(self.stock_df, switch, direction)
+            model = TinyModel(self.stock_df, switch)
             model.run()
 
-            if model.successful_rate >= 80 and len(model.indices) >= 2:
-                print(f'{self.stock_name} - {direction} --> {i + 1}/{len(switches)}: '
-                      f'{model.successful_rate}% among {len(model.indices)} with {model.name}')
-                models.append(model)
+            if model.successful_long_rate >= 80 and len(model.indices) >= 2:
+                print(f'{self.stock_name} {i + 1}/{len(switches)} --> long\t'
+                      f'{model.successful_long_rate}% among {len(model.indices)} with {model.name}')
+                self.long_models.append(model)
+
+            if model.successful_short_rate >= 80 and len(model.indices) >= 2:
+                print(f'{self.stock_name} {i + 1}/{len(switches)} --> short\t'
+                      f'{model.successful_short_rate}% among {len(model.indices)} with {model.name}')
+                self.short_models.append(model)
 
             if (i + 1) % 10_000 == 0:
-                print(f'{self.stock_name} - {direction} --> {i + 1}/{len(switches)}\t'
-                      f'{datetime.now().time()}')
+                print(f'{self.stock_name} {i + 1}/{len(switches)}\t{datetime.now().time()}')
                 # break
-
-    def run(self):
-        self._run('long', self.long_models)
-        self._run('short', self.short_models)
 
     def build_graph(self, fig: go.Figure):
         self.long_models = shrink_models(self.long_models)
@@ -142,7 +148,7 @@ class GiantModel:
 
 if __name__ == '__main__':
     abbrs = [
-        [3, 8],
+        [2, 4, 6],
     ]
 
     for abbr in abbrs:
