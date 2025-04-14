147c258d1db46ec049540b5bcb86388a043707ed
Glen Geng <glen.geng@airwallex.com>
2025-04-15 00:22:48 +0800

    2025-04-15. daily update

---
+++ trading/portfolio.py
@@ -0,0 +1,37 @@
+from conf import *
+
+portfolio = [
+    QQQ,        # ABC is done, call at 433              # in right shoulder, call at 433, target 500          call 433
+    MSFT,       # ABC is done, call at 376              # SHS is acked, call at 372.5                         call 373
+    AAPL,       # A5 is done, call                      # in right shoulder, call at 195, target 234          call 195
+    NVDA,       # A5 is done, call                      # in right shoulder, call at 102, target 135          call 102
+    META,       # neutral, 3 or 5 for A ?               # neutral, still in left shoulder ?
+    AMZN,       # neutral, 3 or 5 for A ?               # bounce back of SHS, good chance to put
+    GOOG,       # A5 is done, call                      # in right shoulder, call at 154, target 191          call 154
+    TSLA,       # neutral, in 4 for A ?                 # neutral, at least 214
+    TSM,        # A5 is done, call                      # in right shoulder, call at 146, target 189          call 146
+    AVGO,       # A5 is done, call                      # up phase of right shoulder is done, wait to put
+    NFLX,       # neutral                               # up phase of right shoulder is done, wait to put
+    JPM,        # neutral                               # bounce back of SHS, good chance to put
+    ASML,       # in C3, put                            # bounce back of Box, put at 683                       put 683
+    LLY,        # in C4, wait to put                    # at the bottom of Box, wait to put >762 or <698
+    WMT,        # A5 is done, in B, be patient          # neutral, 95.22 is the top
+    MA,         # neutral, 3 or 5 for A ?               # neutral
+    VISA,       # A3 is done, in B, wait to call        # neutral
+    HK_0700,    # neutral                               # neutral
+    ORCL,       # ABC is done, call                     # in right shoulder, could call, but margin is tight
+    BAC,        # A5 is done, call                      # in right shoulder, call at 34.5, target 43          call 34.5
+    GS,         # neutral, 3 or 5 for C ?               # bounce back of SHS, put at 524
+    MS,         # neutral, 3 or 5 for C ?               # neutral
+    ADBE,       # ABC is done, call ?                   # neutral, at least 320
+    CVX,        # neutral                               # bottom of box, call at 133                          call 133
+    PLTR,       # neutral, <65 or >95                   # neutral
+    UBER,       # in C, keep put position               # neutral, in right shoulder, which is a triangle
+    NU,         # in C3, keep put position              # in right shoulder, keep put position
+    TCOM,       # in C3, keep put position              # bounce back of SHS, target is 57.7
+    AMD,        # ABC is done, wait to call
+    INTC,       # neutral, in bottom for a long time
+    QCOM,       # ABC is done, wait to call
+    BA,         # neutral, from (2) to (3)
+
+]
