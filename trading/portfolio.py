83f55448ab9e8232a54c16acfc9c294f7c8d2a34
Glen Geng <glen.geng@airwallex.com>
2025-04-16 23:24:11 +0800

    2025-04-16. daily update

--- trading/portfolio.py
+++ trading/portfolio.py
@@ -6,7 +6,7 @@ portfolio = [
     AAPL,       # A5 is done, call                      # in right shoulder, call at 195, target 234          call 195
     NVDA,       # A5 is done, call                      # in right shoulder, call at 102, target 135          call 102
     META,       # neutral, 3 or 5 for A ?               # neutral, still in left shoulder ?
-    AMZN,       # neutral, 3 or 5 for A ?               # bounce back of SHS, good chance to put
+    AMZN,       # neutral, 3 or 5 for A ?               # bounce back of SHS, put at 194
     GOOG,       # A5 is done, call                      # in right shoulder, call at 154, target 191          call 154
     TSLA,       # neutral, in 4 for A ?                 # neutral, at least 214
     TSM,        # A5 is done, call                      # in right shoulder, call at 146, target 189          call 146
@@ -30,18 +30,22 @@ portfolio = [
     PLTR,       # neutral, <65 or >95                   # neutral
 
     AMD,        # ABC is done, wait to call
-    INTC,       # neutral, in bottom for a long time
+    INTC,       # neutral, in bottom for a long time    # bottom of box, call
     QCOM,       # ABC is done, wait to call
     BA,         # neutral, from (2) to (3)
     UBER,       # in C, keep put position               # neutral, in right shoulder, which is a triangle
     SHOP,       # in C5, put
     PDD,        # in C, put
 
-    COIN,       # A5 is done, call
+    PYPL,                                               # in right shoulder, call at 57, target 67
+    COIN,       # A5 is done, call                      # in right shoulder, call at 150, target 280
     NU,         # in C3, keep put position              # in right shoulder, keep put position
 
     TCOM,       # in C3, keep put position              # bounce back of SHS, target is 57.7
     SEA,                                                # neutral, in right shoulder
     DELL,       # ABC is done, call
     HOOD,       # in C, put
+
+    XPEV,       # in 5, call
+    ERIC,       # in 5, call
 ]
